ba6dc199e16133b4ec2f0ddd713dfa4dbb28a16b
dknoops <dimitry.knoops@student.ehb.be>
2021-01-23 14:14:30 +0100

    Updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ export default function Footer() {
     <footer className="footer">
       <div id="terms-of-use">
         <p>
-          <a href="/terms-of-use">Terms of Use</a> &#8226;{" "}
+          <a href="#">Terms Of Use</a> &#8226;{" "}
           <a href="/privacy-policy">Privacy Policy</a>
         </p>
       </div>
-      <p id="copyright">&copy; 2020 Dimitry Knoops</p>
+      <p id="copyright">&copy; 2021 Dimitry Knoops</p>
     </footer>
   );
 }
